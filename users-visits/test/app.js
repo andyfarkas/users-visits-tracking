3a1ba62ec157e6a066813438b0c4bbb0030d31a3
Andrej Farkas <andrej.farkas@dixonsretail.com>
2016-02-15 14:26:18 +0100

    - added better looking tests

--- users-visits/test/app.js
+++ users-visits/test/app.js
@@ -8,15 +8,94 @@ const createVisistsFake = require('./fakes/visits');
 const assert = require('assert');
 
 describe('Application', function() {
-    describe('when started', function() {
-        it('should create new visit on user_joined event', function(done) {
+    describe('when running and user_joined event received with new user', function() {
+        it('should create new visit', function(done) {
             const mongo = createMongoFake();
             const eventsBus = createEventsBusFake();
             const users = createUsersFake();
             const visits = createVisistsFake();
             visits.save = function(visitToSave) {
-                assert.deepEqual(visitToSave.username, 'a new user');
-                done();
+                try {
+                    assert.deepEqual(visitToSave.username, 'a new user');
+                    done();
+                } catch (err) {
+                    done(err);
+                }
+            };
+            const app = createApp(
+                eventsBus,
+                mongo,
+                visits,
+                users
+            );
+            app.start().then(function() {
+                eventsBus.publish('user_joined', {
+                    username: 'a new user'
+                });
+            });
+        });
+
+        it('should create new online user with last visit attached', function(done) {
+            const mongo = createMongoFake();
+            const eventsBus = createEventsBusFake();
+            const users = createUsersFake();
+            const visits = createVisistsFake();
+            const expectedVisit = {
+                username: 'a new user'
+            };
+            visits.create = function() {
+                return expectedVisit;
+            };
+            users.save = function(userToSave) {
+                try {
+                    assert.deepEqual(userToSave.username, 'a new user');
+                    assert.deepEqual(userToSave.lastVisit, expectedVisit);
+                    assert.deepEqual(userToSave.isOnline, true);
+                    done();
+                } catch (err) {
+                    done(err);
+                }
+            };
+            const app = createApp(
+                eventsBus,
+                mongo,
+                visits,
+                users
+            );
+            app.start().then(function() {
+                eventsBus.publish('user_joined', {
+                    username: 'a new user'
+                });
+            });
+        });
+    });
+
+    describe('when running and user_joined event received with existing user', function() {
+        it('should attach last visit to that user and put them online', function(done) {
+            const mongo = createMongoFake();
+            const eventsBus = createEventsBusFake();
+            const users = createUsersFake();
+            const visits = createVisistsFake();
+            const expectedVisit = {
+                username: 'an existing user'
+            };
+            const expectedUser = {
+                username: 'an existing user',
+                isOnline: false
+            };
+            visits.create = function() {
+                return expectedVisit;
+            };
+            users.addExpectedRecord('an existing user', expectedUser)
+            users.save = function(userToSave) {
+                try {
+                    assert.deepEqual(userToSave.username, 'an existing user');
+                    assert.deepEqual(userToSave.lastVisit, expectedVisit);
+                    assert.deepEqual(userToSave.isOnline, true);
+                    done();
+                } catch (err) {
+                    done(err);
+                }
             };
             const app = createApp(
                 eventsBus,
